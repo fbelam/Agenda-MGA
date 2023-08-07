45d2414d3e7b0a2b86f6181b932e2a911e8c94bd
fbelam <36649120+fbelam@users.noreply.github.com>
2023-08-07 20:58:28 -0300

    Add files via upload

---
+++ Agenda/agenda.js
@@ -0,0 +1,9 @@
+function novoEvento() {
+    var novoEvento = document.getElementById('novoEvento');
+    novoEvento.classList.remove('d-none');
+}
+
+function ocultarNovoevento() {
+    var novoEvento = document.getElementById('novoEvento');
+    novoEvento.classList.add('d-none');
+}
